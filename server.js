0d7849828a07149448e0106392ca357260fd6edf
Lara Samuelson <lara.samuelson@gmail.com>
2020-12-05 07:09:11 -0800

    prep for deploy

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const movieData = require('./movieData.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -24,7 +25,7 @@ app.use(function validateBearerToken(req, res, next) {
 app.get( '/movie', function handleGetMovies(req, res){
     const {genre, country, vote}= req.query
     let response = movieData
-    console.log('moviedata')
+ 
     if (genre) {
         response = response.filter(movie =>
           movie.genre.toLowerCase().includes(genre.toLowerCase())
@@ -43,8 +44,17 @@ app.get( '/movie', function handleGetMovies(req, res){
     res.json(response)
 })
 
-const PORT = 8000
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
 })
+
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {})
